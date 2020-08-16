c75b3c0ac2f39ab405e67b02a86f6806b197ba7c
Anderson <anderson.cardenas.r@gmail.com>
2020-08-16 16:12:38 -0500

    uu

---
+++ rutamaya/cuadro.py
@@ -0,0 +1,15 @@
+import pygame
+
+class Interfaz_Cuadro(pygame.sprite.Sprite):
+    def __init__(self):
+        pass
+
+    def dibujar(self):
+        pass
+
+    def mover(self):
+        pass
+
+    def saludar(self):
+         print("al menos el cuadro")
+
