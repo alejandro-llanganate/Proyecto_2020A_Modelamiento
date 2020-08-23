4415352d86fc4ee1d6bac03c9cd368589eaaf37b
alejandrollanganate <luis.llanganate@epn.edu.ec>
2020-08-23 11:54:03 -0500

    subida de imagenes del cuadro vida

--- src/k.py
+++ src/k.py
@@ -11,23 +11,31 @@ screen = pygame.display.set_mode(settings["tamañoVentana"])
 myfont = pygame.font.SysFont("monospace", 15)
 
 # render text
-label = myfont.render("Some text!", 1, (0, 0, 0))
 score = 5
 
 start_ticks = pygame.time.get_ticks()  # starter tick
 while True:  # mainloop
-    
-    
-    
-    
+    myfont = pygame.font.SysFont("arial", 60)
     seconds = (pygame.time.get_ticks()-start_ticks) / 1000
-    if seconds == 1.0:
+    if seconds <= 1.0:
+        label = myfont.render("3", 1, (255, 255, 255))
+        screen.blit(label, (100, 100))
         print(3)
-    if seconds == 2.0:
+    if seconds <= 2.0 and seconds > 1.0:
+        screen.blit(label, (100, 100))
+        label = myfont.render("2", 1, (255, 255, 255))
+        screen.blit(label, (100, 100))
         print(2)
-    if seconds == 3.0:
+    if seconds <= 3.0 and seconds > 2.0:
+        label = myfont.render("1", 1, (255, 255, 255))
+        screen.blit(label, (100, 100))
         print(1)
-    if seconds == 4.0:
-        print("Go")
+    if seconds <= 4.0 and seconds > 3:
+        label = myfont.render("GO", 1, (255, 255, 255))
+        screen.blit(label, (100, 100))
+        print("GO")
+
+    # initialize font; must be called after 'pygame.init()' to avoid 'Font not Initialized' error
+    # render text
 
     pygame.display.update()
