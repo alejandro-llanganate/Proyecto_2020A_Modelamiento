68774fd4e6cd3aeb91b64e54f57c19291b4eceb1
alejandrollanganate <luis.llanganate@epn.edu.ec>
2020-09-08 14:14:25 -0500

    Comentarios en fabricas.py

--- src/ruta/fabricas.py
+++ src/ruta/fabricas.py
@@ -1,3 +1,9 @@
+#=============================================================================================
+#                               JUEGO RUTA MAYA - Version 0.9
+#                         Interfaz Fábrica y Clase FábricaRandómica 
+# Implementado por: Alejandro Llanganate, Anderson Cárdenas, Henrry Cordovillo y David Moreno
+#=============================================================================================
+
 import pygame
 import random
 from ruta.obstaculos import *
@@ -10,17 +16,20 @@ class FabricaObstaculos:
 
 class FabricaRandomica(FabricaObstaculos):
     def crearObstaculo(self):
-        listaObstaculos = []
-        for i in range (32):
-            aleatorio = random.randrange(1,4)
+        obstaculosAleatorios = []
+        numeroObstaculos = 32
+
+        for i in range (numeroObstaculos):
+            aleatorio = random.randrange(1,4) # En randrange el 4 es excluido por lo que el número aleatorio estaría entre 1 a 3
             if aleatorio == 1:
                 obstaculo = ObstaculoA()
-                listaObstaculos.append(obstaculo)
+                obstaculosAleatorios.append(obstaculo)
             elif aleatorio == 2:
                 obstaculo = ObstaculoB()
-                listaObstaculos.append(obstaculo)
+                obstaculosAleatorios.append(obstaculo)
             elif aleatorio == 3:
                 obstaculo = ObstaculoC()
-                listaObstaculos.append(obstaculo)
-        return listaObstaculos
+                obstaculosAleatorios.append(obstaculo)
+        
+        return obstaculosAleatorios # retorna una lista de obstáculos randómicos
     
